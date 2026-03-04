84778496b53d5f38d7645aefc7497a29028c0d06
kugua5201~ <2756427564@qq.com>
2026-03-04 10:37:17 +0800

    备份一些基本逻辑

---
+++ Services/Logs/ILog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasurementSoftware.Services.Logs
+{
+    public interface ILog
+    {
+        void Info(string message);
+
+        void Warn(string message);
+
+        void Error(string message);
+
+        void Debug(string message);
+    }
+}
---
+++ Services/Logs/Log.cs
@@ -0,0 +1,41 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasurementSoftware.Services.Logs
+{
+    public class Log : ILog
+    {
+
+        private readonly Logger _log;
+
+        public Log()
+        {
+            _log = LogManager.GetCurrentClassLogger();
+             MultiProtocol.Services.IIndustrialProtocol.IndustrialProtocol.LoggerFactory= _log;
+        }
+
+        public void Info(string message)
+        {
+            _log.Info(message);
+        }
+
+        public void Warn(string message)
+        {
+            _log.Warn(message);
+        }
+
+        public void Error(string message)
+        {
+            _log.Error(message);
+        }
+
+        public void Debug(string message)
+        {
+            _log.Debug(message);
+        }
+    }
+}
